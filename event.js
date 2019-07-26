ca60804e5313047e4a2aa32d5e49ad63a3221e13
strogaia <nadejda.screbliucova@gmail.com>
2019-07-26 15:14:20 +0300

    Add event.js

---
+++ event.js
@@ -0,0 +1,174 @@
+function showPager( page ){
+    document.body.innerHTML = ' '
+    if( page == 1 ){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">[1]</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 2){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">[2]</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 3){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">[3]</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 4){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">[4]</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 5){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">[5]</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 6){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">[6]</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 7){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">[7]</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 8){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">[8]</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 9){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">[9]</button>
+            <button onclick="showPager(10)">10</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    } else if( page == 10){
+        document.write(`
+        <p align="center">
+            <button onclick="showPager()" >&lt;</button>
+            <button onclick="showPager(1)">1</button>
+            <button onclick="showPager(2)">2</button>
+            <button onclick="showPager(3)">3</button>
+            <button onclick="showPager(4)">4</button>
+            <button onclick="showPager(5)">5</button>
+            <button onclick="showPager(6)">6</button>
+            <button onclick="showPager(7)">7</button>
+            <button onclick="showPager(8)">8</button>
+            <button onclick="showPager(9)">9</button>
+            <button onclick="showPager(10)">[10]</button>
+            <button onclick="showPager()">&lt;</button>
+        </p>
+        `)
+    }
+}
